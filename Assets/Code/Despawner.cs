31e73a2974730b76cdf65254b04c20c56c5015cc
Kalle Heinonen <kalle.heinonen@cs.tamk.fi>
2016-09-16 11:15:01 +0300

    Spawning refined, added Despawner

---
+++ Assets/Code/Despawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Runner {
+    public class Despawner : MonoBehaviour {
+        [SerializeField] private SpawnerController _spawnerController;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(_spawnerController == null || !_spawnerController.AddToPool(other.gameObject))
+            {
+                Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
